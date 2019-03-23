d4cb32210c9de6ddf2d271ac229cdfeb21834b54
feby <febyjemskey@gmail.com>
2019-03-23 15:06:04 +0700

    hapus file login superadmin

--- superadmin/admin_hapus.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include '../koneksi.php';
-
-$id = $_GET['id'];
-
-mysqli_query($koneksi, "DELETE FROM admin WHERE id='$id'")or die("s");
-
-header("location:admin.php");
--- superadmin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-session_destroy();
-
-header("location:../superadmin.php?alert=logout");
-?>
